67a391ff749b1a8c150f4010bc54dd156552bc9a
Johann Knechtel <johann@nyu.edu>
2021-10-07 12:38:49 +0400

    added ITC DEF files; added helper script

---
+++ toys/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lib=~/work/libraries/Nangate_45nm/NangateOpenCellLibrary.lef
+
+for layer in 8 6 3 1
+do
+	for file in `ls *.def`
+	do
+		bench=${file%.*}
+		log=$bench"_M"$layer".log"
+		opens=$bench"_M"$layer"_open_pins.log"
+
+#./extract -i $file --cell_lef $lib --metal $layer -o $log &
+
+		echo "Total # of open pins: " > $opens
+		cat $log | grep "Location" | wc -l >> $opens
+
+		zip "M"$layer".zip" $log $opens
+	done
+done
